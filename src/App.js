f7a12b41aff6e154ad71ab9013d782dad0905f41
SumanDas3001 <sumandas3001@gmail.com>
2019-08-31 14:17:27 +0530

    Chnage App.js to class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome</h1>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return(
+      <h1>welcome</h1>
+    )
+  }
 }
 
 export default App;

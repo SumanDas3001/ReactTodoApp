50e98aeb33887e2c3c5453361c2241c5b8508f75
SumanDas3001 <sumandas3001@gmail.com>
2019-09-01 19:25:41 +0530

    Display Todos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Todos from './Todos'
 
 class App extends React.Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play the cricket'}
+    ]
+  }
   render(){
     return(
-      <h1>welcome</h1>
-    )
+      <div className='todo-app container'>
+        <h1 className='center blue-text'>Todos</h1>
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
   }
 }
 
 export default App;
+
+
